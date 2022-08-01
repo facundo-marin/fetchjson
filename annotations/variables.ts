f9076a223879e339f4ddc01ea765c138fbed0e84
Facundo Marin <facundo.marin@moxit.net>
2022-08-01 15:51:39 -0300

    26.16. Annotations Around Functions.

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -16,3 +16,14 @@ let truths: boolean[] = [true, false, true, false, false, false];
 //classes
 class Car {}
 let car: Car = new Car();
+
+//Object literal
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+};
+
+//Function
+const longNumber: (i: number) => void = (i: number) => {
+  console.log(i);
+};
