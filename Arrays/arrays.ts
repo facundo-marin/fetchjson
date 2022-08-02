49f9e25cd586e580c134964046c32cf5ab9f12b0
Facundo Marin <facundo.marin@moxit.net>
2022-08-02 11:26:27 -0300

    26.28. arrays in Typescript

---
+++ Arrays/arrays.ts
@@ -0,0 +1,5 @@
+const carMakers: string[] = ['ford', 'toyota', 'chevy'];
+const data = [new Date(), new Date()];
+
+const carsByMake: string[][] = [];
+const carbyMake = [['ford'], ['chevy'], ['toyota']];
