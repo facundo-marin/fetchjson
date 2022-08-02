46e9c2583988428ba93698fc82c933b8254452ff
Facundo Marin <facundo.marin@moxit.net>
2022-08-02 12:54:29 -0300

    mend

--- Interfaces/interfaces.ts
+++ Interfaces/interfaces.ts
@@ -1,17 +1,17 @@
 const oldCivic = {
   name: 'civic',
   year: 2000,
-  broke: true,
+  broken: true,
 };
 
 const printVehicle = (vehicle: {
   name: string;
   year: number;
-  broke: boolean;
+  broken: boolean;
 }): void => {
   console.log(`Name: ${vehicle.name}`);
   console.log(`Year: ${vehicle.year}`);
-  console.log(`Broken?: ${vehicle.broke}`);
+  console.log(`Broken?: ${vehicle.broken}`);
 };
 
 printVehicle(oldCivic);
