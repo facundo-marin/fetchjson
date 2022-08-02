8fcedb54a0381bcb9329f7e3037a152c71986114
Facundo Marin <facundo.marin@moxit.net>
2022-08-02 12:23:52 -0300

    26.33. Tuplas in action

---
+++ Tuples/tuples.ts
@@ -0,0 +1,14 @@
+const drink = {
+  color: 'brown',
+  carbonated: true,
+  sugar: 40,
+};
+
+//Type alias
+type Drink = [string, boolean, number];
+
+//Sin usar Type alias
+// const pepsi: [string, boolean, number] = ['brown', true, 40];
+const pepsi: Drink = ['brown', true, 40];
+const sprite: Drink = ['clear', true, 40];
+const tea: Drink = ['brown', false, 0];
