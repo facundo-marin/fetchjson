7aebc86eb7b9aa7eeeaf965c9d948eae88b04b36
Facundo Marin <facundo.marin@moxit.net>
2022-08-02 11:42:04 -0300

    26.29. Why tiped arrays

--- Arrays/arrays.ts
+++ Arrays/arrays.ts
@@ -3,3 +3,15 @@ const data = [new Date(), new Date()];
 
 const carsByMake: string[][] = [];
 const carbyMake = [['ford'], ['chevy'], ['toyota']];
+
+//Help with inference when extracting values
+const car1 = carMakers[0];
+const myCar = carMakers.pop();
+
+// Prevent incompatible values
+carMakers.push(100);
+
+// help with map
+carMakers.map((car: string): string => {
+  return car;
+});
