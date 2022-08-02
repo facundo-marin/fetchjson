d4c53a9014bd6436163f3e1b51eecfe75b7577e2
Facundo Marin <facundo.marin@moxit.net>
2022-08-02 11:00:22 -0300

    26.26. Destructuring with Annotations

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -31,3 +31,21 @@ const throwError2 = (message: string): string => {
 
   return message;
 };
+
+const forecast = {
+  date: new Date(),
+  weather: 'sunny',
+};
+
+const logWeather = ({
+  date,
+  weather,
+}: {
+  date: Date;
+  weather: string;
+}): void => {
+  console.log(date);
+  console.log(weather);
+};
+
+logWeather(forecast);
